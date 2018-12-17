55ad8adf07bc190ae635617e5065f30fc6ed67ad
rita.radovics <rita.radovics@agillic.com>
2018-12-17 18:52:19 +0100

    FIRST WORKING PROTOTYPE BITCHES

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,28 +1,27 @@
 <template>
   <div class="myMusic">
-    <button class="btn" v-if='$root.playlistId != "" ' v-on:click="createPlaylist">Create</button>
-    {{ $root.db }}
-    <iframe
-      :src="frameSrc"
-      width="300"
-      height="380"
-      frameborder="0"
-      allowtransparency="true"
-      allow="encrypted-media"
-    ></iframe>
-    <h2>Here we will have songs</h2>
-    <div class="myTracks" v-for="artist in myArtists">Genres:
-      <span class="myArtists" v-for="genre in artist.genres">{{ genre + ' '}}</span>
-      <br>
-      Name: {{artist.name}}
-      <br>
-      ID: {{artist.id}}
-    </div>
-    <!--<div class="myTracks" v-for="track in myMusic">
-      <span class="myArtists" v-for="artist in track.artists">{{artist.name}}</span>
-      {{ track.name }}
-      <img :src='track.album.images[2].url'>
-    </div>-->
+    <button class="btn" @click.prevent="showModal = true" >Create</button>
+    <!-- {{ $root.db }} -->
+    <div @close="showModal = false">
+        <transition name="modal">
+          <div class="modal-mask"  v-if="showModal">
+            <div class="modal-wrapper">
+              <div class="box col-sm-3 modal-container">
+                <a class="close-modal" id="show-modal" @click.prevent="showModal = false"><i class="close-icon fa fa-times "></i></a>
+                <h3 class="subtitle">Share this key</h3>
+                <ol>
+                  <li>Copy the key and share it with your friends</li>
+                  <li>Wait for them to accept</li>
+                  <li>Come back to this app and see your Playlist</li>
+                </ol>
+                <h3 class='key'>demo</h3>
+                <span class='buttonwrapper'><button class='createPlaylistButton' v-if='$root.playlistId != "" ' v-on:click="createPlaylist">Create Playlist</button></span>
+                <a class='cancelbutton' @click.prevent="showModal = false">Cancel</a>
+              </div>
+            </div>
+          </div>
+        </transition>
+      </div>
   </div>
 </template>
 
@@ -42,9 +41,9 @@ export default {
       songsArray2: [],
       uriArray: [],
       playlistTopSongs: [],
-      frameSrc: null,
       finalarray: [],
-    };
+      showModal: false,
+    }
   },
   mounted() {
     this.getUserInfo();
@@ -169,17 +168,10 @@ export default {
         )
         .then(function(response) {
           that.pushedTracks = response.data;
-          alert("Playlist created!");
-          that.updateFrame()
+          alert("Playlist created!" + that.createdPlaylist.id);
+          that.$router.push({ path: '/playlist', query: { id: that.createdPlaylist.id } });
         });
     },
-    updateFrame() {
-      this.frameSrc =
-        "https://open.spotify.com/embed/user/1175743727/playlist/" +
-        this.createdPlaylist.id;
-        this.$router.push('/playlist');
-    },
-   
   }
 };
 </script>
@@ -193,5 +185,70 @@ export default {
 }
 .myMusic {
   padding-top: 15vh;
+}
+.modal-container{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 40px;
+  height: 70vh;
+  background-color: #76C5CA;
+}
+.close-modal{
+  margin-right: -30px;
+  margin-top: -30px;
+}
+.createPlaylistButton{
+  color: #1F1A4E;
+  padding: 5px 15px;
+  font-size: 1.2em;
+  border-radius: 1.875em;
+  height: 2.5em;
+  background-color: #FFFFFF;
+  cursor: pointer;
+  width: 10em;
+  border: none;
+}
+.buttonwrapper{
+  padding-top: 20px;
+  text-align: center;
+}
+.cancelbutton{
+  padding-top: 10px;
+  color: white;
+  font-size: 1.2em;
+  cursor: pointer;
+  text-align: center;
+}
+h1{
+  text-transform: uppercase;
+
+}
+h3{
+  color: #1F1A4E;
+  font-size: 2em;
+  text-align: left;
+  margin: 0;
+}
+h4{
+  font-size: 1.5em;
+  color: white;
+  text-align: left;
+  margin: 0;
+  margin-bottom: 25px;
+}
+.key{
+  text-align: center;
+}
+.modal-fade-enter-active {
+  transition: all .3s ease;
+}
+.modal-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.modal-fade-enter, .modal-fade-leave-to
+/* .modal-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
 }
 </style>
